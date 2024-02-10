b03f5597b95bba503baa34199ebd58310e4f60a1
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2024-02-10 19:59:02 +0500

    implement projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,34 @@
+import { projects } from '@/const/projects'
+import Link from 'next/link'
+import { BiLogoGithub } from 'react-icons/bi'
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="w-full h-screen">
+      <div className="w-full max-w-md mx-auto h-96 flex flex-col gap-4 items-center justify-center px-6">
+        <div className="w-24 aspect-square rounded-full bg-muted" />
+        <div className="w-fit h-fit flex items-center flex-col justify-center">
+          <span className="text-center text-accent-foreground text-2xl font-bold">YZ13</span>
+          <span className="text-sm text-muted-foreground text-center">Fullstack-developer</span>
+        </div>
+        <span className='text-center text-sm text-muted-foreground'> Hi -_- </span>
+      </div>
+      <div className="w-full h-fit flex items-center justify-center gap-2 py-6">
+        <Link href='https://github.com/yz13-env' className="w-6 aspect-square flex items-center justify-center">
+          <BiLogoGithub size={24} />
+        </Link>
+        <div aria-label="link-to-social" className="w-6 aspect-square flex items-center justify-center" />
+      </div>
+      <div className="w-full max-w-md mx-auto px-6">
+        {
+          projects.map(project =>
+            <Link href={project.link} key={project.projectId} className="w-full flex flex-col border-b py-2">
+              <span className="font-medium text-accent-foreground text-start">{ project.name }</span>
+              <span className="text-sm text-muted-foreground text-start">{ project.description }</span>
+            </Link>
+          )
+        }
+      </div>
+    </main>
+  )
 }
---
+++ const/projects.ts
@@ -0,0 +1,58 @@
+type CDN_LINK = `https://cdn.darkmaterial.space/${string}`
+type ProjectLink = `https://${string}.${string}` | `https://${string}.${string}/${string}`
+type ProjectIcon = CDN_LINK
+type ProjectsGroup = {
+  groupId: string
+  name: string
+  description: string
+  icon?: ProjectIcon
+}
+type Project = {
+  projectId: string
+  type: 'app' | 'package'
+  name: string
+  description: string
+  status: 'dev' | 'prod' | 'pre-prod' | 'planned'
+  disabled: boolean
+  link: ProjectLink
+  icon: ProjectIcon
+  createdAt: number // Нужно для обозначения бейджиком "новое"
+  tags: string[]
+  partOf: string[] // Нужно для перечисления принадлежности к другим проекта
+}
+export const type_variants = (type: Project['type']) => type === 'app' ? "приложение" : "библиотека"
+export const projects: Project[] = [
+  {
+    projectId: "darkmaterial",
+    type: "app",
+    name: "Darkmaterial",
+    description: "Приложение для управления аккаунтом",
+    status: "prod",
+    link: "https://darkmaterial.space",
+    partOf: [ "darkmaterial-group" ],
+    disabled: false,
+    createdAt: 0,
+    icon: 'https://cdn.darkmaterial.space/dm/icons/dm-star-dark.svg',
+    tags: []
+  },
+  {
+    projectId: "frame",
+    type: "app",
+    name: "Frame",
+    description: "Публикуйте свои работы, и используйте профиль как портфолио",
+    status: 'prod',
+    link: "https://frame.darkmaterial.space",
+    partOf: [ "darkmaterial-group" ],
+    disabled: false,
+    createdAt: 0,
+    icon: 'https://cdn.darkmaterial.space/dm/icons/frame-dark.svg',
+    tags: []
+  }
+]
+export const projects_groups: ProjectsGroup[] = [
+  {
+    name: "Darkmaterial",
+    description: "Большой проект :)",
+    groupId: "darkmaterial-group",
+  }
+]
