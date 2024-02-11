380a6dba1b3a81122fdfd760bb06151869b57d5e
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2024-02-11 11:20:31 +0500

    updated project component

--- app/_components/project.tsx
+++ app/_components/project.tsx
@@ -2,6 +2,7 @@ import { projects, projects_groups, status_variants } from "@/const/projects"
 import { Repo } from "@/types/repo"
 import Image from "next/image"
 import Link from "next/link"
+import { PiDotOutlineFill } from "react-icons/pi";
 
 type Props = {
   repo: Repo
@@ -10,11 +11,20 @@ const Project = ({ repo }: Props) => {
   const extra = projects.find(project => project.projectId === repo.name)
   const project_group = extra ? projects_groups.find(group => extra.partOf.includes(group.groupId)) : null
   // TODO - группу и статус поместит в массив и вывести их с соединением в виде точки
+  const top_tags = [ project_group?.name, extra ? status_variants(extra?.status) : null ].filter(item => item)
   return (
     <Link href={repo.html_url} key={repo.id} className="w-full flex flex-col border-b hover:border-b-primary transition-colors py-2 relative">
-      <div className='relative w-full h-fit flex items-center justify-start gap-2'>
-        { project_group && <span className="text-xs w-fit text-muted-foreground">{ project_group?.name }</span> }
-        { extra && <span className="text-xs text-muted-foreground">{ status_variants(extra.status) }</span> }
+      <div className='relative w-full h-fit flex items-center justify-start'>
+        {
+          top_tags.map((tag, i, arr) =>
+            <>
+              <span className="text-xs w-fit text-muted-foreground" key={tag}>{tag}</span>
+              { i < arr.length - 1 && <PiDotOutlineFill className="text-muted-foreground" /> }
+            </>
+          )
+        }
+        {/* { project_group && <span className="text-xs w-fit text-muted-foreground">{ project_group?.name }</span> } */}
+        {/* { extra && <span className="text-xs text-muted-foreground">{ status_variants(extra.status) }</span> } */}
       </div>
       <div className='relative w-full h-fit flex items-center justify-start'>
         {
