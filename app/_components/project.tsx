e2858cfdadfd843bef7841d6aed31824243db831
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2024-03-16 09:40:22 +0500

    update project components

--- app/_components/project.tsx
+++ app/_components/project.tsx
@@ -1,62 +1,41 @@
-import { projects, projects_groups, status_variants } from "@/const/projects"
-import { Repo } from "@/types/repo"
-import Image from "next/image"
-import Link from "next/link"
-import { Fragment } from "react";
-import { BiLogoGithub } from "react-icons/bi";
+import { DocDefaultProject } from "@darkmaterial/api";
+import Image from "next/image";
+import Link from "next/link";
 import { MdOpenInNew } from "react-icons/md";
-import { PiDotOutlineFill } from "react-icons/pi";
 
 type Props = {
-  repo: Repo
+  repo: DocDefaultProject
 }
 const Project = ({ repo }: Props) => {
-  const extra = projects.find(project => project.projectId === repo.name)
-  const project_group = extra ? projects_groups.find(group => extra.partOf.includes(group.groupId)) : null
-  // TODO - группу и статус поместит в массив и вывести их с соединением в виде точки
-  const top_tags = [project_group?.name, extra ? status_variants(extra?.status) : null].filter(item => item)
-  const link = extra ? extra.link : ''
-  if (!extra) return null
   return (
     <div className="w-full flex flex-col border hover:border-primary rounded-lg p-3 transition-colors relative group">
-      <Link target="_blank" href={link} className="w-6 aspect-square rounded-full bg-primary text-primary-foreground absolute -top-2.5 -right-2.5 z-10 hidden group-hover:flex items-center justify-center">
-        <MdOpenInNew size={14} />
-      </Link>
+      {
+        repo.link &&
+        <Link
+          target="_blank"
+          href={repo.link}
+          className="w-6 aspect-square rounded-full bg-primary text-primary-foreground absolute -top-2.5 -right-2.5 z-10 hidden group-hover:flex items-center justify-center"
+        >
+          <MdOpenInNew size={14} />
+        </Link>
+      }
       <div className="w-full h-fit flex items-center justify-between mb-2">
         <div className="w-fit h-fit flex items-center gap-2">
           {
-            extra &&
+            repo.iconUrl &&
             <Image
               className="aspect-square shrink-0"
               priority
-              src={extra.icon}
+              src={repo.iconUrl}
               width={24}
               height={24}
               alt='repo-logo'
             />
           }
-          <div className="flex flex-col justify-center">
-            <div className='relative w-full h-fit flex items-center justify-start'>
-              {
-                top_tags.map((tag, i, arr) =>
-                  <Fragment key={tag + '-' + i}>
-                    <span className="text-xs w-fit text-muted-foreground">{tag}</span>
-                    {i < arr.length - 1 && <PiDotOutlineFill className="text-muted-foreground" />}
-                  </Fragment>
-                )
-              }
-              {/* { project_group && <span className="text-xs w-fit text-muted-foreground">{ project_group?.name }</span> } */}
-              {/* { extra && <span className="text-xs text-muted-foreground">{ status_variants(extra.status) }</span> } */}
-            </div>
-            <span className="font-medium text-sm text-accent-foreground text-start inline-flex gap-1 items-center">{extra ? extra.name : repo.name}</span>
-          </div>
+          <span className="font-medium text-sm text-accent-foreground text-start capitalize">{repo.name}</span>
         </div>
       </div>
-      <Link href={repo.html_url} className="px-2 py-1 rounded-full bg-muted flex w-fit items-center gap-1 text-xs my-1">
-        <BiLogoGithub size={16} />
-        {repo.full_name}
-      </Link>
-      <span className="text-sm line-clamp-1 text-muted-foreground text-start">{repo.description || extra?.description || 'Без описания'}</span>
+      <span className="text-sm line-clamp-2 text-muted-foreground text-start">{repo.description || 'Без описания'}</span>
     </div>
   )
 }
--- app/_components/repos-grid.tsx
+++ app/_components/repos-grid.tsx
@@ -1,27 +1,27 @@
 import { user as user_api } from '@/api/user'
-import { projects } from '@/const/projects'
-import Project from './project'
 import { Button } from '@/components/ui/button'
+import { default_api } from '@darkmaterial/api'
 import Link from 'next/link'
 import { BiRightArrowAlt } from 'react-icons/bi'
+import Project from './project'
 
 const ReposGrid = async () => {
   const repos = await user_api.repos.get()
-  const matched = repos.filter(repo => !!projects.find(project => project.projectId === repo.name))
+  const projects = await default_api.all()
   const github_link = "https://github.com/yz13-env"
+  console.log(projects)
   return (
     <>
       <div className='w-full flex items-center mt-12 justify-between max-w-xl mx-auto px-6'>
-        <span className='text-sm text-muted-foreground'>Показано {matched.length}/{repos.length}</span>
+        <span className='text-sm text-muted-foreground'>Показано {projects.length}/{repos.length}</span>
         <Button variant='outline' className='gap-2' asChild>
           <Link href={github_link}>Посмотреть все <BiRightArrowAlt size={16} /></Link>
         </Button>
       </div>
       <div className="w-full max-w-xl grid md:grid-cols-2 grid-cols-1 auto-rows-auto gap-2 mx-auto px-6 py-4">
         {
-          matched
-            .sort()
-            .map(repo => <Project key={repo.id} repo={repo} />)
+          projects
+            .map(repo => <Project key={repo.doc_id} repo={repo} />)
         }
       </div>
     </>
